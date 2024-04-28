4dc8e530e00accddcb78c923c1bf16e1e74bd5f9
kub891 <kuba891891@gmail.com>
2024-04-29 01:25:49 +0200

    2

--- bookfun.py
+++ bookfun.py
@@ -1,14 +1,38 @@
-import csv
+from datetime import date
 import pandas as pd
-import os
 def add_book():
-    file_path = os.path.abspath('Library/book.csv')
-    with open(file_path, 'r') as file:
-    # Odczytanie zawartości pliku
-     zawartosc = file.read()
+    existing_data = pd.read_csv('./Library/book.csv')
+    author = input('Podaj Autora: ')
+    title = input('Podaj tytul ksiazki: ')
+    pages = int(input('Podaj ilosc stron: '))
+    new_data = pd.DataFrame({'ID': [int(existing_data.iloc[-1]['ID'])+1], 'AUTHOR': [author], 'TITLE': [title],'PAGES': [pages], 'CREATED': [date.today()], 'UPDATED': [date.today()]})
+    updated_data = pd.concat([existing_data, new_data], ignore_index=True)
+    updated_data.to_csv('./Library/book.csv', index=False)
 
-# Wyświetlenie zawartości pliku
-    print(zawartosc)
+def remove_book_id(book_id):
+    try:
+        existing_data = pd.read_csv('./Library/book.csv')
+        if book_id not in existing_data['ID'].values:
+            raise ValueError("Książka o podanym ID nie istnieje.")
+        updated_data = existing_data[existing_data['ID'] != book_id]
+        print(updated_data)
+        updated_data.to_csv('./Library/book.csv', index=False)
+        print("Ksiazka zostala pomyslnie usunieta.")
+    except FileNotFoundError:
+        print("Plik CSV nie został znaleziony.")
+    except Exception as e:
+        print("error:", e)
 
-# Dodanie nowych danych do istniejących danych
-#nowa_ramka_danych = pd.concat([existing_data, nowe_dane], ignore_index=True)
+def remove_book_title(book_title):
+    try:
+        existing_data = pd.read_csv('./Library/book.csv')
+        if book_title not in existing_data['TITLE'].values:
+            raise ValueError("Książka o podanym tytule nie istnieje.")
+        updated_data = existing_data[existing_data['ID'] != book_title]
+        print(updated_data)
+        updated_data.to_csv('./Library/book.csv', index=False)
+        print("Książka została pomyślnie usunięta.")
+    except FileNotFoundError:
+        print("Plik CSV nie został znaleziony.")
+    except Exception as e:
+        print("error:", e)
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import bookfun
+
 def main():
-    input()
-
-
+    print('Co chcesz zrobic?\n1) Dodaj Ksiazke do bazy\n2) Usun ksiazke z bazy po numerze ID\n3) Usun ksiazke z bazy po tytule')
+    selection = int(input('Wybierz numer odpowiadajacy danej funkcji: '))
+    if(selection == 1):
+        bookfun.add_book()
+    elif(selection == 2):
+        book_id = int(input('Podaj ID ksiazki: '))
+        bookfun.remove_book_id(book_id)
+    elif(selection == 3):
+        book_title = input('Podaj tytul ksiazki: ')
+        bookfun.remove_book_title(book_title)
 
 
 if __name__ == '__main__':
